6973178e39d0e98be15f1f79899116b93c9593d4
Next innovation <166608182+Vivekkaklotar@users.noreply.github.com>
2026-03-31 12:29:25 +0000

    31th march

---
+++ C++/FirstCPP.cpp
@@ -0,0 +1,13 @@
+#include<iostream>  //input output stream
+using namespace std;  //standard namespace
+int main()
+{
+    int num1,num2;
+    cout<<"Enter num 1: ";
+    cin>>num1;
+    cout<<"Enter num 2: ";
+    cin>>num2;
+    int sum=num1+num2;
+    cout<<"Sum: "<<sum;
+    return 0;
+}
